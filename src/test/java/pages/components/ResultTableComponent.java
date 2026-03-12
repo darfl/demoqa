7aaeb90aaafdd1142ff416550a3f66e281a58e98
Diana Darfl <diana.darfl@gmail.com>
2026-03-12 21:09:33 +0500

    added checkResult, Enums and 2 tests

---
+++ src/test/java/pages/components/ResultTableComponent.java
@@ -0,0 +1,34 @@
+package pages.components;
+
+import com.codeborne.selenide.SelenideElement;
+import enums.ResultTableEnums;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ResultTableComponent {
+
+    private final SelenideElement
+            resultTitle = $("#example-modal-sizes-title-lg"),
+            resultTable = $(".table-responsive");
+
+    public ResultTableComponent checkTitle() {
+        resultTitle.shouldHave(exactText("Thanks for submitting the form"));
+        return this;
+    }
+
+    public ResultTableComponent checkTable(ResultTableEnums key, String value) {
+        System.out.println(key + " " + value);
+        resultTable.$(byText(key.toString())).parent()
+                .shouldHave(text(value));
+        return this;
+    }
+
+    public ResultTableComponent checkTitleMissing() {
+        resultTitle.shouldNotBe(visible);
+        return this;
+    }
+}
