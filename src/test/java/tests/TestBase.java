c071c50b87ee6523c5bc9f6373eca60636d0e9d0
Diana Darfl <diana.darfl@gmail.com>
2026-04-01 00:55:49 +0500

    added info in build.gradle for jenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,6 +15,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.headless = true;
     }
 
     @AfterEach
