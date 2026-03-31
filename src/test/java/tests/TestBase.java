034e3427223ae9e10250e6a5f049306552e6e946
Diana Darfl <diana.darfl@gmail.com>
2026-04-01 01:51:03 +0500

    added info in build.gradle for jenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,14 +18,13 @@ public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
-
     static void setUpBeforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = true;
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("version", "122");
+        Configuration.browserVersion = System.getProperty("version", "128");
         //Configuration.remote = System.getProperty("remoteUrl");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
